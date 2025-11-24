0e2907273d4c46dc2424b18fb04ea57a66202538
Temirgali Alisher <keykibatyr@Temirgalis-MacBook-Pro.local>
2025-11-24 17:41:02 +0500

    redo

---
+++ internal/controllers/templte.go
@@ -0,0 +1,7 @@
+// package controllers
+
+// import "net/http"
+
+// type Template interface {
+// 	ExecuteTemp(w http.ResponseWriter, r *http.Request, data interface{})
+// }
---
+++ internal/controllers/users.go
@@ -0,0 +1,10 @@
+// package controllers
+
+// type User struct {
+// 	Templates struct {
+// 		New    Template
+// 		SignIn Template
+// 	}
+
+// 	UserService *models.UserService
+// }
